dd830f6320e3e63ce2be5acbdfdf7b76b2f4ed24
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-02-01 15:28:04 -0600

    Added TriggerWriting Script and TypeWriter

---
+++ TriggerWriting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerWriting : MonoBehaviour {
+	
+	public string wholeText = "This is a simple text. Is this fast enough for you Sam?";
+
+
+
+	// Use this for initialization
+	void Start () {
+		wholeText = "This is a simple text. Is this fast enough for you Sam?";
+		Debug.Log (wholeText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider Cube)
+	{
+		Debug.Log ("Cheese!");
+		wholeText = "Hello World!";
+		Debug.Log (wholeText + "LOL");
+	}
+}
---
+++ TypeWriter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TypeWriter : MonoBehaviour
+{
+	private const float WAIT_TIME = 0.05f;
+	
+	private float waitTimer = 0.0f;
+	public string wholeText = "";
+	private string typewriterText = "";
+    private string s = "";
+	private int currentIndex = 0;
+
+	private bool clickActivation = false;
+
+
+	void Start(){
+		wholeText = "Still Pulling From TypeWriter";
+	}
+
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Mouse0)) { 
+			clickActivation = true;
+		}
+
+		if (clickActivation)
+		{
+			//if (Input.GetMouseButton (LEFT_MOUSE_BUTTON))
+			waitTimer += Time.deltaTime;
+			wholeText = GameObject.Find ("Basic").GetComponent<TriggerWriting> ().wholeText;
+			
+			if (waitTimer > WAIT_TIME && currentIndex < wholeText.Length) {            
+				typewriterText += wholeText [currentIndex];
+				waitTimer = 0.0f;
+				++currentIndex;
+			}
+
+			if (currentIndex == wholeText.Length)
+			{
+				clickActivation = false;
+			}
+
+		}
+	}
+	
+	void OnGUI()
+	{
+		s = GUI.TextArea(new Rect(0.0f, 0.0f, Screen.width, Screen.height/2), typewriterText);
+        if (Input.GetKey(KeyCode.E))
+        {
+            s = "";
+            Debug.Log("You Pressed E");
+        }
+	}
+}
