3ec2e0df26b8376ce7d9292c5f543c7fda104e5f
gebharsj <gebharsj@gmatc.matc.edu>
2016-02-01 15:24:17 -0600

    Added the player movement script

---
+++ PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveX;
+    public float moveY;
+    public float movementSpeed;
+    public Rigidbody player;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal") * movementSpeed;
+        moveY = Input.GetAxis("Vertical") * movementSpeed;
+        player.velocity = new Vector3(moveX, moveY, 0);
+	}
+}
