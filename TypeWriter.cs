3945351b83168576525a2e1ee07db45ff1a2010d
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-02-02 12:51:53 -0600

    Updated TypeWriter So It Clears After Click

--- TypeWriter.cs
+++ TypeWriter.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class TypeWriter : MonoBehaviour
 {
-	private const float WAIT_TIME = 0.05f;
+	private const float WAIT_TIME = 0.00001f;
 	
 	private float waitTimer = 0.0f;
 	public string wholeText = "";
+	public string otherText = "";
 	private string typewriterText = "";
-    private string s = "";
 	private int currentIndex = 0;
 
 	private bool clickActivation = false;
+
+
+	private bool cheese = false;
+
+
 
 
 	void Start(){
@@ -23,6 +28,7 @@ public class TypeWriter : MonoBehaviour
 	{
 		if (Input.GetKey (KeyCode.Mouse0)) { 
 			clickActivation = true;
+			cheese = false;
 		}
 
 		if (clickActivation)
@@ -40,6 +46,9 @@ public class TypeWriter : MonoBehaviour
 			if (currentIndex == wholeText.Length)
 			{
 				clickActivation = false;
+				currentIndex = 0;
+				cheese = true;
+
 			}
 
 		}
@@ -47,11 +56,19 @@ public class TypeWriter : MonoBehaviour
 	
 	void OnGUI()
 	{
-		s = GUI.TextArea(new Rect(0.0f, 0.0f, Screen.width, Screen.height/2), typewriterText);
-        if (Input.GetKey(KeyCode.E))
-        {
-            s = "";
-            Debug.Log("You Pressed E");
-        }
+		if (cheese) 
+		{
+			otherText = typewriterText;
+			if (Input.GetMouseButton(0))
+			{
+
+			typewriterText = "";
+			}
+			GUI.TextArea (new Rect (0.0f, 0.0f, Screen.width, Screen.height / 3), otherText);
+		}
+		else
+		{
+			GUI.TextArea (new Rect (0.0f, 0.0f, Screen.width, Screen.height / 3), typewriterText);
+		}
 	}
 }
